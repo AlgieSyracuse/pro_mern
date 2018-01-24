8761eeef757053d6148a98b0c9eb0cc862b56acc
ywang305 <ywang305@syr.edu>
2018-01-24 00:53:01 -0500

    polypill: let brower run es2015

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 /* any modification in this file can be watched by "npm run watch" */
 
 var contentNode = document.getElementById('contents');
-var component = <h1>Hello World, I am back!</h1>;        // A simple component, written in JSX
+//var component = <h1>Hello World, I am back!</h1>;        // A simple component, written in JSX
+
+const continents = ['Africa','America','Asia','Australia','Europe'];
+const message = continents.map(c => `Hello ${c}!`).join(' ');
+
+const component = <p>{message}</p>;           // A simple component, written in JSX
 ReactDOM.render(component, contentNode);      // Render the component inside the content Node
--- static/App.js
+++ static/App.js
@@ -1,9 +1,18 @@
+'use strict';
+
 /* any modification in this file can be watched by "npm run watch" */
 
 var contentNode = document.getElementById('contents');
+//var component = <h1>Hello World, I am back!</h1>;        // A simple component, written in JSX
+
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+var message = continents.map(function (c) {
+  return 'Hello ' + c + '!';
+}).join(' ');
+
 var component = React.createElement(
-  'h1',
+  'p',
   null,
-  'Hello World, I am back!'
+  message
 ); // A simple component, written in JSX
 ReactDOM.render(component, contentNode); // Render the component inside the content Node
