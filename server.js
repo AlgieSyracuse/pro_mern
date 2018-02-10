05410ef7ca9e802974e22bbdd6ce065f4d52e00e
ywang305 <ywang305@syr.edu>
2018-02-10 00:27:12 -0500

    GET POST api, error handle

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+/*
+    any change in this code will result in nodejs restart, via package "nodemon", 
+    referring to "npm start" script -> start: "nodemon -w server.js server.js"
+*/
+
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
+
+// middlewares to use
+  // express built-in middleware
 app.use(express.static('static'));
+  // body-parser module, parse HTTP body
+app.use(bodyParser.json());
 
 // pretending data in server
 const issues = [
@@ -20,7 +31,64 @@ const issues = [
 // route matching by Express, only matching http GET request
 app.get('/api/issues', (req, res)=>{
     const metadata = {total_count: issues.length};
-    res.json({_metadata: metadata, records: issues});
+    res.json({_metadata: metadata, records: issues}); // client: curl -s URL | json_pp
+    //res.send('Hello World!\n');
+});
+
+
+// posted data validation
+const validIssueStatus = {
+    New: true,
+    Open: true,
+    Assigned: true,
+    Fixed: true,
+    Verified: true,
+    Closed: true,
+};
+  
+const issueFieldType = {
+    id: 'required',
+    status: 'required',
+    owner: 'required',
+    effort: 'optional',
+    created: 'required',
+    completionDate: 'optional',
+    title: 'required',
+};
+  
+function validateIssue(issue) {
+    for (const field in issueFieldType) {
+      const type = issueFieldType[field];
+      if (!type) {
+        delete issue[field];
+      } else if (type === 'required' && !issue[field]) {
+        return `${field} is required.`;
+      }
+    }
+  
+    if (!validIssueStatus[issue.status])
+      return `${issue.status} is not a valid status.`;
+  
+    return null;
+  }
+
+
+// POST REST api
+app.post('/api/issues', (req, res)=>{
+    const newIssue = req.body;
+    newIssue.id = issues.length+1;
+    newIssue.created = new Date();
+    if(!newIssue.status) newIssue.status = 'New';
+
+    // handling err
+    const err = validateIssue(newIssue);
+    if(err) {
+        res.status(422).json({message: `\ninvalid request: \n ${err}`}); // client will callback the message
+        return;
+    }
+
+    issues.push(newIssue);
+    res.json(newIssue); // return data newIssue to client
 });
 
 
